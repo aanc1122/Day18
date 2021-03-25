41dbed8dc7c6fb7032b859a23790ccb73aa5693f
aanc1122 <amelanuhanovic997@gmail.com>
2021-03-26 00:14:35 +0100

    Generator functions and Gigi Arena Web Page

---
+++ ex05/src/index.js
@@ -0,0 +1,25 @@
+let shopingList = new Map([
+    ['Banana', 3],
+    ['Pineapple', 5],
+    ['Pear', 2],
+    ['Carrot', 10],
+    ['Apple', 1.5]
+]);
+
+let arrKey = [];
+let arrValue = [];
+
+for (let item of shopingList) {
+arrKey.push(item[0]);
+console.log("groceries: ", item[0]);
+}
+for (let item of shopingList) {
+    arrValue.push(item[1]);
+    console.log("amount: ", item[1]);
+    }
+for (let item of shopingList) {
+    
+    console.log(item);
+        }
+
+module.exports = { shopingList, arrKey, arrValue };
